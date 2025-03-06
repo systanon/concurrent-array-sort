38afb56d074433e7abf0dde83e7ea77524e195de
Serhii Tustanovskyi <serhii@MacBook-Air-Serhii.local>
2025-03-06 15:51:18 +0100

    feat: decomposition

--- main.go
+++ main.go
@@ -19,17 +19,7 @@ func main() {
 	}
 
 	chunks := utils.Split(arr, PARTS)
-	var wg sync.WaitGroup
-	sortedParts := make(chan []int, PARTS)
-
-	for i := 0; i < PARTS; i++ {
-		wg.Add(1)
-		go sortPartition(chunks[i], &wg, sortedParts)
-
-	}
-	wg.Wait()
-	close(sortedParts)
-
+	sortedParts := cuncurrency(chunks, sortPartition)
 	var result []int
 	for part := range sortedParts {
 		result = append(result, part...)
@@ -41,4 +31,19 @@ func main() {
 func sortPartition(arr []int, wg *sync.WaitGroup, sortedParts chan<- []int) {
 	defer wg.Done()
 	sortedParts <- arr
+}
+
+func cuncurrency(chunks [][]int, cb func(arr []int, wg *sync.WaitGroup, sortedParts chan<- []int)) chan []int {
+	var wg sync.WaitGroup
+	sortedParts := make(chan []int, PARTS)
+
+	for i := 0; i < PARTS; i++ {
+		wg.Add(1)
+		go cb(chunks[i], &wg, sortedParts)
+
+	}
+	wg.Wait()
+	close(sortedParts)
+
+	return sortedParts
 }
